78431869c2a06ce5ce4851d21f93e922259a8475
zzes <kuanglong0312@sina.com>
2019-02-19 16:02:32 +0800

    35

---
+++ 35.search-insert-position.js
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (42.42%)
+ * Total Accepted:    27.8K
+ * Total Submissions: 65.6K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 
+ * 你可以假设数组中无重复元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let location = nums.indexOf(target)
+  if(location > -1) {
+    return location
+  } else {
+    nums.push(target)
+    nums.sort((function(a, b) {
+      return a - b;
+    }))
+    let location = nums.indexOf(target)
+    return location
+  }
+};
+searchInsert([3,5,7,9,10], 8)
